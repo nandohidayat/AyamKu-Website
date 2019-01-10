17c3f6c803fc68a339b4e20fdad7aaefbc6f4aab
nando hidayat <mnandohidayat@gmail.com>
2019-01-10 08:54:49 +0000

    add detail penjualan with item detail

--- displayjual.php
+++ displayjual.php
@@ -1,5 +1,6 @@
 <?php
-	include "koneksi_ip.php"
+	include "koneksi_ip.php";
+	$fn  = $_POST['fn'];
 ?>
 <HTML>
 <HEAD>
@@ -7,11 +8,11 @@
 
 <script language="javascript">
 function tanya() {
-if (confirm ("Apakah Anda yakin akan menghapus barang ini ?")) {
-	return true;
-} else {
-	return false;
-}
+	if (confirm ("Apakah Anda yakin akan menghapus barang ini ?")) {
+		return true;
+	} else {
+		return false;
+	}
 }
 </script>
 </HEAD>
@@ -40,6 +41,7 @@ if (confirm ("Apakah Anda yakin akan menghapus barang ini ?")) {
                                  <th><i class="icon_mobile"></i> User</th>
 								 <th><i class="icon_mobile"></i> Total</th>
 								 <th><i class="icon_calendar"></i> Bayar</th>
+								 <th><i class="icon_cogs"></i> Action</th>
                               </tr>
 
 
@@ -82,17 +84,84 @@ if (confirm ("Apakah Anda yakin akan menghapus barang ini ?")) {
 		<td align='left' >$nama</td>
 		<td align='left'>$user</td>
 		<td align='right'>$total</td>
-		<td align='right'>$bayar</td>";
-		?>
+		<td align='right'>$bayar</td>
 		
-                              </tr>
-	        <?php } ?>
+		<td>
+		    <div class='btn-group'>
+                <button type='button' class='btn btn-success' data-toggle='modal' data-target='#Modal$id_jual'>
+					View Details
+				</button>
+            </div>
+        </td>
+		
+        </tr>";
+	     } ?>
 		</tbody>
                         </table>
                       </section>
                   </div>
               </div>
 		
+		<?php 
 		
+			$sql = mysqli_query ($conn,$query);
+		 	while ($hasil = mysqli_fetch_array ($sql)) {
+				$id_jual = $hasil['id_jual'];
+				$nama = stripslashes ($hasil['nama']);
+				$user = stripslashes ($hasil['user_id']);
+				$total = $hasil['total'];
+				$bayar = $hasil['bayar'];
+				
+				echo '<div class="modal fade" id="Modal'.$id_jual.'" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+						  <div class="modal-dialog" role="document">
+						    <div class="modal-content">
+						      <div class="modal-header">
+						        <h5 class="modal-title" id="ModalLabel">Detail Transaksi</h5>
+						      </div>
+						      <div class="modal-body">
+						        <p><span class="forjual">ID Transaksi</span>: '.$id_jual.'</p>
+						        <p><span class="forjual">Nama Gerai</span>: '.$nama.'</p>
+						        <p><span class="forjual">Nama Pelanggan</span>: '.$user.'</p>
+						        <p><span class="forjual">Pembelian</span>: </p>';
+						        
+						        $query = "SELECT 
+							    			jual.*, gerai.nama 
+							    		FROM 
+							    			jual
+							    		INNER JOIN
+							    			gerai ON jual.kd_gerai = gerai.kd_gerai
+							    		WHERE
+							    			jual.kd_gerai = '".$gerai."'
+							    		ORDER BY 
+							    			jual.id_jual";
+    			
+						        $query1 = "SELECT
+											list_jual.kd_brg, barang.nm_brg, barang.harga_jual
+										FROM
+											list_jual
+										INNER JOIN
+											barang ON list_jual.kd_brg = barang.kd_brg
+										WHERE
+											list_jual.id_jual = '".$id_jual."'";
+								$sql1 = mysqli_query ($conn,$query1);
+								while ($hasil1 = mysqli_fetch_array ($sql1)) {
+									$nm_brg = $hasil1['nm_brg'];
+									$harga_jual = $hasil1['harga_jual'];
+									
+									echo '<p><span class="forjual"></span> '.$nm_brg.'  -  Rp '.$harga_jual.',00</p>';
+								}
+							
+						        echo '<p><span class="forjual">Total</span>: Rp '.$total.',00</p>
+						        <p><span class="forjual">Bayar</span>: Rp '.$bayar.',00</p>
+						        <p><span class="forjual">Kembalian</span>: Rp '.($bayar-$total).',00</p>
+						      </div>
+						      <div class="modal-footer">
+						        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						      </div>
+						    </div>
+						  </div>
+						</div>';
+		 	}
+		?>
 </BODY>
 </HTML>
